54b32e0d2fb2c46994da1698fead528519617e48
Cody Constant <astraeusdev@gmail.com>
2023-02-15 23:54:40 +0000

    feat(hero section): create a basic hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,59 @@
-import { FC } from "react";
-const HeroSection: FC = () => {
-  return <div>Hero Section Stub</div>;
+const HeroSection = () => {
+  return (
+    <div
+      className="
+      w-screen
+      h-screen
+      flex
+      justify-center
+      items-center"
+    >
+      <div
+        className="
+        px-8
+        max-w-2xl"
+      >
+        <p
+          className="
+          text-slate-800
+          text-lg
+          sm:text-xl
+          font-medium"
+        >
+          Hi, my name is
+        </p>
+        <h1
+          className="
+          text-4xl
+          sm:text-5xl
+          font-bold
+          text-zinc-900
+          mt-4"
+        >
+          Cody Constant
+        </h1>
+        <h2
+          className="
+          text-4xl
+          sm:text-5xl
+          font-bold
+          text-zinc-600
+          mt-3"
+        >
+          I build web tools.
+        </h2>
+        <p
+          className="
+          text-zinc-600
+          mt-6
+          sm:text-xl"
+        >
+          I am a software developer specializing in building web based tools to
+          expand business functionality and improve operations.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
