ee0f040164671279df314a0ea5ea23d7b2216022
Cody Constant <astraeusdev@gmail.com>
2023-02-16 04:30:40 +0000

    feat(hoverable): add modifier to query for hover ability of device

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        hoverable: { raw: "(any-hover: hover)" },
+      },
+    },
   },
   plugins: [],
 };
