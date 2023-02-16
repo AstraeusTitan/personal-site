04cdcdd4992e45735dbf28c2815fbccb13106f81
Cody Constant <astraeusdev@gmail.com>
2023-02-16 03:23:23 +0000

    change(hero-section): adjust text alignment and color

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,20 +7,18 @@ const HeroSection = () => {
         className="
         h-screen
         flex
-        justify-center
         items-center"
       >
         <div
           className="
-          px-8
-          max-w-2xl"
+          px-8"
         >
           <p
             className="
-            text-sky-600
+            text-sky-500
             text-lg
             sm:text-xl
-            font-medium"
+            font-bold"
           >
             Hi, my name is
           </p>
@@ -48,7 +46,8 @@ const HeroSection = () => {
             className="
             text-zinc-600
             mt-6
-            sm:text-xl"
+            sm:text-xl
+            max-w-2xl"
           >
             I am a software developer specializing in building web based tools
             to expand business functionality and improve operations.
