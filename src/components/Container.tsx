f79262edc9ec31eaada424ce7d0eeb5c0e1cb708
Cody Constant <astraeusdev@gmail.com>
2023-02-16 01:02:03 +0000

    feat(container): create a container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,34 @@
+import { FC, ReactNode } from "react";
+import { clsx } from "clsx";
+
+type TContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const Outer: FC<TContainerProps> = ({ children, className }) => {
+  return (
+    <div className={clsx("sm:px-8", className)}>
+      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+    </div>
+  );
+};
+
+const Inner: FC<TContainerProps> = ({ children, className }) => {
+  return (
+    <div className={clsx("relative px-4 sm:px-8 lg:px-12", className)}>
+      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+    </div>
+  );
+};
+
+export const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <Outer>
+      <Inner>{children}</Inner>
+    </Outer>
+  );
+};
+
+Container.Outer = Outer;
+Container.Inner = Inner;
